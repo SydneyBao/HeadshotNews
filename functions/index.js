fd5d8ed7a9f1fc7a8194a9939538048bc6083b6f
Sydney Bao <bao.sy@northeastern.edu>
2024-07-17 21:13:43 -0700

    scraper

--- functions/index.js
+++ functions/index.js
@@ -6,12 +6,16 @@ const puppeteer = require("puppeteer-core");
 admin.initializeApp();
 const db = admin.firestore();
 
-exports.onUserCreate = functions.firestore.document("users/{userid}")
-    .onCreate(async (snap, context) => {
-      const url = "https://en.wikipedia.org/wiki/Main_Page";
+exports.scraper = functions
+    .region('us-west2')
+    .runWith({memory: '2GB'})
+    .scraper.schedule("0 0 * * *")
+    .timeZone("America/Los_Angeles")
+    .onRun(async () => {
 
       let browser;
       let page;
+      let docRef;
 
       try {
         browser = await puppeteer.launch({
@@ -22,26 +26,82 @@ exports.onUserCreate = functions.firestore.document("users/{userid}")
         });
 
         page = await browser.newPage();
-        await page.goto(url, {waitUntil: "networkidle0"});
+        
+        // Counter-Strike.net
+        await page.goto('https://www.counter-strike.net/news', { waitUntil: 'networkidle0' });
+        let articles = await page.$$('.blogcapsule_BlogCapsule_3OBoG');
 
-        let imgSource = await page
-            .$eval("#mp-otd #mp-otd-img img", (img) => img.src);
-
-        if (imgSource) {
-          imgSource = imgSource.replace("thumb/", "");
-          const fileExIndex = Math.max(
-              imgSource.indexOf(".jpg/"),
-              imgSource.indexOf(".JPG/"),
-              imgSource.indexOf(".png/"),
-              imgSource.indexOf(".PNG/"),
-          );
-          imgSource = imgSource.substring(0, fileExIndex + 4);
+        for (const a of articles) {
+            const title = await a.$eval('.blogcapsule_Title_39UGs', node => node.innerText);
+            const url = await a.evaluate(node => node.href);
+            const image = await a.$eval('.blogcapsule_Image_Nh_xZ', node => node.style.backgroundImage.slice(5, -2));
+            
+            docRef = await db.collection("news_data").add({
+              title, url, image
+            });
         }
 
-        const docRef = await db.collection("wikipedia_data").add({
-          imgSource,
-        });
+        //htlv
+        await page.goto('https://www.hltv.org/news/archive/2024/May', { waitUntil: 'networkidle0' });
+        articles = await page.$$('.article');
 
+        for (const a of articles) {
+            const title = await a.$eval('.newstext', node => node.innerText);
+            const date = await a.$eval('.newsrecent', node => node.innerText);
+            const url = await a.evaluate(node => node.href);
+            const image = await a.$eval('img', node => node.src);
+            
+            docRef = await db.collection("news_data").add({
+              title, url, image
+            });
+        }
+
+        //dust2
+        await page.goto('https://www.dust2.us/archive?offset=0', { waitUntil: 'networkidle0' });
+        const dateHeaders = await page.$$('.group-header');
+        
+        for (let d of dateHeaders) {
+            const date = await page.evaluate(element => element.textContent, d);
+            
+            const articleSelectorHandle = await page.evaluateHandle(el => el.parentElement, d);
+            const articles = await articleSelectorHandle.$$('.archive-news-item');
+
+            for (let a of articles) {
+                const title = await a.$eval('.news-item-header', element => element.textContent);
+                const url = await a.$eval('a', element => element.href);
+                const image = await a.$eval('img', element => element.src);
+                
+                docRef = await db.collection("news_data").add({
+                  title, url, image
+                });
+            }
+        }
+
+        //dbltap
+        await page.goto('https://www.dbltap.com/leagues/counter-strike-global-offensive', { waitUntil: 'networkidle0' });
+        articles = await page.$$('article');
+
+        for (const a of articles) {
+            let author;
+            try {
+                author = (await page.evaluate(el => el.querySelector('h4').textContent, a)).split('|')[0].trim();
+            } catch {
+                author = (await page.evaluate(el => el.querySelector('h5').textContent, a)).split('|')[0].trim();
+            }
+    
+            if (author !== 'Max Mallow' && author !== 'Alexandra Hobbs' && author !== 'Conner Dejecacion') {
+                const title = await page.evaluate(el => el.querySelector('h3').textContent, a);
+                const date = await page.evaluate(el => el.querySelector('time').textContent, a);
+                const url = await page.evaluate(el => el.querySelector('a').href, a);
+                const imageSelector = 'img';
+                await a.waitForSelector(imageSelector, { timeout: 10000 });
+                const image = await a.$eval(imageSelector, img => img.src);
+                
+                docRef = await db.collection("news_data").add({
+                  title, url, image
+                });
+            }
+        }
         console.log(`Data stored successfully with ID: ${docRef.id}`);
       } catch (error) {
         console.error("Error in Cloud Function:", error);
